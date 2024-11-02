7ffefd42affaefc9c4b9b016d630e71bf3bc7517
Brock Pavelko <bpavelko624@gmail.com>
2024-11-02 01:54:17 -0400

    new design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import Spline from "@splinetool/react-spline";
+import { IconBrandX, IconBrandDiscord, IconBrandGithub } from "@tabler/icons-react";
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen relative">
-      <img src="logo.png" className="w-96 z-10 drop-shadow-2xl"/>
-      <Spline scene="https://prod.spline.design/yiBieDyDt7KOa3-8/scene.splinecode" className="absolute object-cover w-full h-full"/>
+    <div className="flex flex-col items-center justify-center min-h-screen relative">
+      <img src="logo.png" className="w-96 drop-shadow-2xl" />
+      <div className="flex space-x-8 mt-4 z-10 text-[#db4a2b]">
+        <a
+          href="https://x.com/kumo_labs"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon-glow"
+        >
+          <IconBrandX size={48} />
+        </a>
+        <a
+          href="https://discord.gg/wWdYejq25K"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon-glow"
+        >
+          <IconBrandDiscord size={48} />
+        </a>
+        <a
+          href="https://github.com/kumolabs-co"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon-glow"
+        >
+          <IconBrandGithub size={48} />
+        </a>
+      </div>
+      <div className="absolute bottom-0 left-0 p-2 text-gray-600">
+        &copy; {new Date().getFullYear()} kumo labs
+      </div>
     </div>
   );
 }
